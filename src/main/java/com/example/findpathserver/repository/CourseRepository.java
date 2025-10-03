ddc9f10a50d0b3905c4954d7db67d736a8e97478
johnock <8855795@naver.com>
2025-10-03 14:42:00 +0900

    course에 관련된 데이터 삭제

--- src/main/java/com/example/findpathserver/repository/CourseRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.findpathserver.repository;
-
-import com.example.findpathserver.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> { }
